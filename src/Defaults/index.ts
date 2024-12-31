7aad29a7cd5ac1c77144fb0469dd6e56344e1c6c
BOTGITBASH <kk0298156@gmail.com>
2025-01-01 00:00:00 +0700

    Htry

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -90,8 +90,8 @@ export const MEDIA_PATH_MAP: { [T in MediaType]?: string } = {
 	'thumbnail-link': '/mms/image',
 	'product-catalog-image': '/product/image',
 	'md-app-state': '',
+	'md-msg-hist': '/mms/group-history',
 	'md-msg-hist': '/mms/md-app-state',
-	'group-history': '/mms/group-history',
 	'biz-cover-photo': '/pps/biz-cover-photo'
 }
 
